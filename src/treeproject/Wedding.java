e88634df2519cdcdca965a627a0ff5d6e5b00e53
Haralds Plinta <plinta.haralds@gmail.com>
2020-08-18 16:52:35 +0300

    Person

---
+++ src/treeproject/Wedding.java
@@ -0,0 +1,30 @@
+package treeproject;
+
+import java.util.Date;
+
+public class Wedding {
+
+	private Date weddingDate;
+
+	private Couple couple;
+
+	public Wedding(Date date, Couple c) {
+
+		weddingDate = date;
+
+		couple = c;
+	}
+
+	public Date getWeddingDate() {
+
+		return weddingDate;
+
+	}
+
+	public Couple getCouple() {
+
+		return couple;
+
+	}
+
+}
